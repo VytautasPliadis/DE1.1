f0c2643cb0753daf8b59351d393427f85cb8fc29
VytautasPliadis <vytautas.pliadis@gmail.com>
2023-09-29 09:19:23 +0300

    init

--- main.py
+++ main.py
@@ -3,15 +3,14 @@ import random
 from collections import defaultdict
 
 
-def do_on_click():
-    pass
-
+if 'df_data' not in st.session_state:
+    st.session_state.df_data = defaultdict(list)
 
 st.title('DE 1.1')
 st.markdown('A DICE GAME')
 
-if 'df_data' not in st.session_state:
-    st.session_state.df_data = defaultdict(list)
+# Slider for number of payers
+players_num = st.sidebar.slider('SELECT NUMBER OF PLAYERS:', min_value=1, max_value=5)
 
 # Slider for number of dices
 dice_num = st.sidebar.slider('SELECT NUMBER OF DICES:', min_value=1, max_value=5)
@@ -20,21 +19,28 @@ dice_num = st.sidebar.slider('SELECT NUMBER OF DICES:', min_value=1, max_value=5
 dice_slide = st.sidebar.slider('SELECT HOW MANY SIDES A DICE CAN HAVE:', min_value=1, max_value=100)
 
 # Button. Throw the dices
-throw_dice_btn = st.sidebar.button('Click me to trow the dices!', key='button1', use_container_width=True)
+throw_dice_btn = st.button('Click me to trow the dices!', key='button1', use_container_width=True)
+
+# Button. Restart the game
+restart_btn = st.sidebar.button('Apply settings and start new game', key='button2', use_container_width=True)
+if restart_btn:
+    st.session_state.df_data = defaultdict(list)
+    st.session_state.df_data['Player_1'].append(0)
+    st.session_state.df_data['Player_2'].append(0)
+    st.session_state.df_data['Player_3'].append(0)
 
 # Write result
 random_numbers = []
 if throw_dice_btn:
     # st.sidebar.write('Dices were trowed!')
     random_dice_value = random.randint(1, int(dice_slide))
-    st.session_state.df_data['Number of dices'].append(dice_num)
-    st.session_state.df_data['Number of sides'].append(dice_slide)
-    st.session_state.df_data['Score'].append(random_dice_value)
-
-# Button. Restart the game
-restart_btn = st.sidebar.button('Clear the score table', key='button2', use_container_width=True)
-if restart_btn:
-    st.session_state.df_data = defaultdict(list)
+    st.session_state.df_data['Player_1'].append(dice_slide)
+    st.session_state.df_data['Player_2'].append(dice_slide)
+    st.session_state.df_data['Player_3'].append(dice_slide)
+    st.session_state.df_data['Winner'].append('Player_1')
+    # st.session_state.df_data['Number of dices'].append(dice_num)
+    # st.session_state.df_data['Number of sides'].append(dice_slide)
+    # st.session_state.df_data['Score'].append(random_dice_value)
 
 # Table to display results
 st.table(st.session_state.df_data)
